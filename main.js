6c2bd8eab089dd98801f5df19ad5657ad32e8904
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-03-17 18:42:33 -0700

    Update main.js
    completed with all :)

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
 function setup()
 {
   video = createCapture(VIDEO);
@@ -9,7 +14,11 @@ function setup()
 }
 function draw()
 {
-    background('#944c00');
+    background('gray');
+    document.getElementById("square_side").innerHTML = "The width and height of the square are " + difference + "px";
+    fill('orange');
+    stroke('orange');
+    square(noseX, noseY, difference);
 }
 function modelLoaded()
 {
@@ -21,4 +30,11 @@ function gotPoses(results)
     {
         console.log(results);
     }
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX + " noseY = " + noseY);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
 }
